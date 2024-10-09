9352c6b27033ebe9c92b526cdf867309ab8e7ed5
Christopher Vu <christophervu03@gmail.com>
2024-10-09 09:37:41 -0500

    Creating Table

--- answers.sh
+++ answers.sh
@@ -7,9 +7,9 @@ UPDATE students SET Points = 500 WHERE Name='Basma';
 UPDATE students SET Points = 100 WHERE Name='Alex';
 
 CREATE TABLE graduates(
-    ID INTEGER  PRIMARY KEY AUTOINCREMENT,
-    Name  TEXT NOT NULL UNIQUE,
-    Age INTEGER,
+	ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	Name TEXT NOT NULL UNIQUE,
+	Age INTEGER,
 	Gender TEXT,
 	Points INTEGER,
 	Graduation TEXT
@@ -17,7 +17,7 @@ CREATE TABLE graduates(
 
 INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
 UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
-DELETE FROM graduates WHERE Name='Layal';
+DELETE FROM students WHERE Name='Layal';
 
 SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
 SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
